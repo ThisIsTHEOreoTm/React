619313b3931656de3d074c86c698f38cf3d90360
sofi <sofianemnmn@gmail.com>
2025-06-10 19:29:02 +0100

    Nothing really

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Design from './Design'
 import './App.css'
 
 function App() {
@@ -22,14 +23,17 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save and save to test HMR
         </p>
+        <h3>Welcome to Vite + React</h3>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Design />
     </>
   )
 }
 
 export default App
+
---
+++ src/Design.jsx
@@ -0,0 +1,9 @@
+let Design = () => {
+  return (
+    <div className="design">
+      <h2>Design Component</h2>
+      <p>This is a simple design component.</p>
+    </div>
+  )
+}
+export default Design
