3a426a9fdd0e398a2fac80b42fa0c8b11527ba73
sofi <sofianemnmn@gmail.com>
2025-06-11 23:33:23 +0100

    Add some new files

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ function App() {
   return (
     <>
       <Header />
+      <div className='Father'>
       <div className="container2">
         <Post />
         <Post />
         <Post />
         <Post />
         <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+      <div className="container3">
+      </div>
       </div>
     </>
   )
--- src/component/post.jsx
+++ src/component/post.jsx
@@ -4,7 +4,7 @@ let post = ()=> {
     return (
         <div className="post">
             <h2 className="post-title">This is The Post Title</h2>
-            <hr />
+            <hr/>
             <p className="post-content">This in the Post body </p>
         </div>
     );
