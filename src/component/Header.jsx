d28071135684d59e0ac317ddfa8dcd5166ddb21e
sofi <sofianemnmn@gmail.com>
2025-06-11 23:08:36 +0100

    Add some new files

---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css';
+let Header = () => {
+  return (
+    <div className="container">
+    <h2 className='Head' >Tarmeez Academy</h2>
+    </div>
+    
+  )
+}
+export default Header
+
+
+
---
+++ src/component/post.jsx
@@ -0,0 +1,12 @@
+import "./post.css";
+
+let post = ()=> {
+    return (
+        <div className="post">
+            <h2 className="post-title">This is The Post Title</h2>
+            <hr />
+            <p className="post-content">This in the Post body </p>
+        </div>
+    );
+}
+export default post;
