76b8985d6b823e256354ad704a3d8b842d6a92dd
sofi <sofianemnmn@gmail.com>
2025-06-13 16:39:52 +0100

    Add Mini project3

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,44 @@ import Bottonss from './component/bottons'
 import './App.css'
 
 function App() {
+  const posts = [
+    { title: "My Academy", content: "This is the 20 academy", children: <h1>20</h1> },
+    { title: "hello world", content: "This is the hello world article" },
+    { title: "post3", content: "this is the body of post 3" }
+  ];
+  const postElements = posts.map((post, index) => (
+    <Post 
+      key={index} 
+      title={post.title} 
+      content={post.content} 
+    >
+    {post.children}
+    </Post>
+  ));
+  const bottons = [
+    { title: "New", children: "🌟🌟🌟" },
+    { title: "most review", children: <img src={flowerImg} alt="flower" className="img" /> },
+    { title: "special review", children: <img src={flowerImg2} alt="flower" className="img" /> }
+  ];
+  const bottonElements = bottons.map((button, index) => (
+    <Bottonss 
+    key={index} 
+    title={button.title}> 
+       <div>
+        {button.children}
+       </div>
+      </Bottonss>
+  ));
+  
   return (
     <>
       <Header />
       <div className='Father'>
       <div className="container2">
-        <Post title="My Academy"  content ="This is the 20 academy" > <h1>20</h1> </Post>
-        <Post title="hello world" content="This is the hello world article" />
-        <Post title="post3"       content="this is the body of post 3" />
+        {postElements}
       </div>
       <div className={"container3"}>
-        <Bottonss title="New"> 
-         <div>
-          <span>🌟🌟🌟</span>
-         </div>
-        </Bottonss>
-        
-        <Bottonss title="most review"> 
-         <div>
-          <img src={flowerImg} alt="flower" className="img" />
-         </div>
-        </Bottonss>
-        
-        <Bottonss title="special review">
-          <div>
-          <img src={flowerImg2} alt="flower" className="img" />
-         </div>
-
-        </Bottonss>
+        {bottonElements}
       </div>
       </div>
     </>
